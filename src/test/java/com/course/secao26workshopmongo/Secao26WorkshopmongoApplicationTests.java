b2e228cb6f91dddd69a5c8927fbcd231e06bcb2e
Jefferson Hita <jefflaiho@hotmail.com>
2022-04-24 20:49:05 -0300

    Projeto criado

---
+++ src/test/java/com/course/secao26workshopmongo/Secao26WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.secao26workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao26WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
