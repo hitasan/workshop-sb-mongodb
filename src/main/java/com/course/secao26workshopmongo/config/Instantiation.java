64af86924c4cecd27bcb6123a873b3d4291e8a67
Jefferson Hita <jefflaiho@hotmail.com>
2022-04-24 21:45:39 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/course/secao26workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.course.secao26workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.course.secao26workshopmongo.domain.User;
+import com.course.secao26workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll(); // Apagando todos dados da base de inicio para teste completo
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+
+}
