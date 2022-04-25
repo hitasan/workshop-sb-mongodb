0304fc496324b6301febe144f4cb1f455791308c
Jefferson Hita <jefflaiho@hotmail.com>
2022-04-24 21:12:43 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/course/secao26workshopmongo/domain/User.java
@@ -0,0 +1,66 @@
+package com.course.secao26workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	// Atributos da classe
+	private String id;
+	private String name;
+	private String email;
+	
+	// Associações da classes
+	
+	// Construtores
+	public User() {
+	}
+
+	public User(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	
+	// Getters / Setters
+	public String getId() { return id; }
+	public void setId(String id) { this.id = id; }
+
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+
+	
+	// Hashcode / Equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		User other = (User) obj;
+
+		return Objects.equals(id, other.id);
+	}
+
+
+	// Metodo toString
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+}
