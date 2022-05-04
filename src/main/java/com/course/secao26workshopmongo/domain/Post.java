4f3550bb3901ffc0c19ff8dd8343fac5bb538262
Jefferson Hita <jefflaiho@hotmail.com>
2022-05-03 23:31:24 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/course/secao26workshopmongo/domain/Post.java
+++ src/main/java/com/course/secao26workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.course.secao26workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.course.secao26workshopmongo.dto.AuthorDTO;
+import com.course.secao26workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,7 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -59,21 +63,14 @@ public class Post implements Serializable {
 		this.title = title;
 	}
 
-	public String getBody() {
-		return body;
-	}
+	public String getBody() { return body; }
+	public void setBody(String body) { this.body = body; }
+	
+	public AuthorDTO getAuthor() { return author; }
+	public void setAuthor(AuthorDTO author) { this.author = author; }
 
-	public void setBody(String body) {
-		this.body = body;
-	}
-	
-	public AuthorDTO getAuthor() {
-		return author;
-	}
-	
-	public void setAuthor(AuthorDTO author) {
-		this.author = author;
-	}
+	public List<CommentDTO> getComments() { return comments; }
+	public void setComments(List<CommentDTO> comments) { this.comments = comments; }
 
 
 	@Override
