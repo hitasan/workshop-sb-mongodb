a2bad6abcbb64d65f47bab34cafd7214eedf87bb
Jefferson Hita <jefflaiho@hotmail.com>
2022-05-03 23:48:55 -0300

    Consulta simples com query methods

--- src/main/java/com/course/secao26workshopmongo/services/PostService.java
+++ src/main/java/com/course/secao26workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.course.secao26workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repos.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repos.findByTitleContaining(text);
 	}
 }
