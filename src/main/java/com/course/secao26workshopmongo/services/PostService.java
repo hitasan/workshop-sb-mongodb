3413cd81831b2abb4440aa7d4032724747f399a1
Jefferson Hita <jefflaiho@hotmail.com>
2022-05-03 23:19:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/course/secao26workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.course.secao26workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.secao26workshopmongo.domain.Post;
+import com.course.secao26workshopmongo.repository.PostRepository;
+import com.course.secao26workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repos; 
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repos.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
