b180ea1536f816c0f0c39bd886b5414edc605e8a
Jefferson Hita <jefflaiho@hotmail.com>
2022-05-04 00:25:05 -0300

    Consulta com varios criterios

--- src/main/java/com/course/secao26workshopmongo/resources/PostResource.java
+++ src/main/java/com/course/secao26workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.course.secao26workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,8 +31,18 @@ public class PostResource {
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);	// Decodificando texto para passagem pela URL
-
+		
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body( list ); 
+	}
+
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text, @RequestParam(value = "minDate", defaultValue = "") String minDate, @RequestParam(value = "maxDate", defaultValue = "") String maxDate)  {
+		text = URL.decodeParam(text);	// Decodificando texto para passagem pela URL
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body( list ); 
 	}
 }
--- src/main/java/com/course/secao26workshopmongo/services/PostService.java
+++ src/main/java/com/course/secao26workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.course.secao26workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,10 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		//return repos.findByTitleContainingIgnoreCase(text);
 		return repos.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repos.fullSearch(text, minDate, maxDate);
 	}
 }
